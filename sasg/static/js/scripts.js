7af3daedf9d2001093e44596d3442caeade324c0
Pedrosanchez20 <126877515+Pedrosanchez20@users.noreply.github.com>
2024-03-07 08:02:03 -0500

    Mantenibilidad

--- sasg/static/js/scripts.js
+++
@@ -1,9 +0,0 @@
-function openModal(id) {
-    var modal = document.getElementById('modal' + id);
-    modal.style.display = 'block';
-}
-
-function closeModal(id) {
-    var modal = document.getElementById('modal' + id);
-    modal.style.display = 'none';
-}
