0d918e4898417ede8792a6178caf07f59c8bf1a0
SantiagoSena <115506922+SantiagoSena@users.noreply.github.com>
2024-03-11 11:36:54 -0500

    Update

--- asago/settings.py
+++ asago/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'sasg',
     
     'django_filters',
+    
+    
 ]
 
 MIDDLEWARE = [
--- sasg/filters.py
+++ sasg/filters.py
@@ -1,6 +1,7 @@
-"""import django_filters 
+import django_filters
 from django import forms
-from .models import Producto, Venta, Pedido, Compra, Usuarios
+
+from .models import Compra, Pedido, Producto, Usuarios, Venta
 
 
 #-------------FILTRO DE PRODUCTOS-------------------------------
@@ -157,4 +158,3 @@ class UsuariosFilter(django_filters.FilterSet):
         model = Usuarios
         fields = ['idusuario', 'rol', 'nombres' , 'apellidos', 'estado']
     
-"""
