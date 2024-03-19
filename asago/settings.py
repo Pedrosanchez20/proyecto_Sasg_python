69864b201d69cffeb9bf9cfb602112f8720245b6
Pedrosanchez20 <126877515+Pedrosanchez20@users.noreply.github.com>
2024-03-19 15:45:06 -0500

    Update

--- asago/settings.py
+++ asago/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q4+&0$en8@!#_qh_8c%0d#9+qsqfx)^fy28ex715b*m*a+0jgj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = [
 #     'able-sensible-llama.ngrok-free.app'
 # ]
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'asago.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'asago',
+        'NAME': 'asagoprueba',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
